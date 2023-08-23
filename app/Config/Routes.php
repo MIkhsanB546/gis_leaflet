e75c72036af06c24036f757de67719aabbd023a4
MIkhsanB546 <mikhsan546@gmail.com>
2023-08-23 13:13:48 +0700

    commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,7 +29,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Lokasi::pemetaanLokasi');
+$routes->get('/', 'AuthController::login');
 
 $routes->get('/pdf', 'PdfController::index');
 $routes->get('/pdf/generate', 'PdfController::generate');
